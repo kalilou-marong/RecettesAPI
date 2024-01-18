2f5517aa061215364627a6c7f1ec246bf85e8822
kalilou-marong <kalilou.marong@hotmail.fr>
2024-01-18 14:44:13 +0100

    Création du CategorieController

---
+++ Controllers/CategorieController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RecettesAPI_HBKMAM.Data;
+
+namespace RecettesAPI_HBKMAM.Controllers
+{
+    [Route("api/categorie")]
+    [ApiController]
+    public class CategorieController : ControllerBase
+    {
+        private readonly RecettesAPIContext _context;
+
+        public CategorieController(RecettesAPIContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetCategorie()
+        {
+            var categorie = _context.Categorie.ToList();
+            return Ok(categorie);
+        }
+    }
+}
